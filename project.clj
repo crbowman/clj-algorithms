36d3e794be9409496ce99a06521acb121a02de1b
crbowman <c.bowman1711@gmail.com>
2018-10-31 23:30:32 -0400

    Remove unused dependency (incanter)

--- project.clj
+++ project.clj
@@ -2,6 +2,5 @@
   :description "A collection of algorithm implementations for learning purposes"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [incanter "1.9.3"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :plugins [[cider/cider-nrepl "0.18.0"]])
