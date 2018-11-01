cf5f4beb5cf3286c073ea8ff0aef4c1163f49bc3
crbowman <c.bowman1711@gmail.com>
2018-10-31 21:35:42 -0400

    Change clojure version (1.9.1 is unreleased)

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "A collection of algorithm implementations for learning purposes"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.1"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [incanter "1.9.3"]]
   :plugins [[cider/cider-nrepl "0.18.0"]])
