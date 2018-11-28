3a9b670c1e939a8f69ada64112a50da29420d965
narwally <c.bowman1711@gmail.com>
2018-11-28 17:59:36 -0500

    Talking clock whitespace

--- src/clj_algorithms/interview/talking-clock.clj
+++ src/clj_algorithms/interview/talking-clock.clj
@@ -52,14 +52,13 @@
 (def hours {0 "twelve" 1 "one" 2 "two" 3 "three" 4 "four" 5 "five"
             6 "six" 7 "seven" 8 "eight" 9 "nine" 10 "ten" 11 "eleven"})
 
-(def tens {0 "oh" 2 "twenty" 3 "thirty" 4 "fourty" 5 "fifty"})
-
 (def teens {0 "ten" 1 "eleven" 2 "twelve" 3 "thirteen" 4 "fourteen"
             5 "fifteen" 6 "sixteen" 7 "seventeen" 8 "eighteen" 9 "nineteen"})
 
+(def tens {0 "oh" 2 "twenty" 3 "thirty" 4 "fourty" 5 "fifty"})
+
 (def ones {1 "one" 2 "two" 3 "three" 4 "four" 5 "five"
            6 "six" 7 "seven" 8 "eight" 9 "nine"})
-
 
 (defn hour [h]
   (hours (rem h 12)))
